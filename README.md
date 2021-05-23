777580294421f112c3f8e61bd1e17545b5ff63ef
reetpriye <reetpriye@gmail.com>
2021-05-23 21:52:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Xylophone
+
+Play beats to beat your boredom
+This app is built using Flutter.
---
+++ android/app/src/main/kotlin/reetpriye/me/xylophone/MainActivity.kt
@@ -0,0 +1,6 @@
+package reetpriye.me.xylophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
